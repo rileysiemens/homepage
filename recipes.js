9808f29071725f7d27dbf71d30ec368c262b84b9
Riley Siemens <hi@rileysiemens.com>
2024-07-09 20:49:50 -0500

    made a whole bunch of improvements

---
+++ recipes.js
@@ -0,0 +1,80 @@
+recipes = [
+	{
+		"name": "Neapolitan Pizza Dough",
+		"ingredients": [
+			{
+				"name": "water",
+				"amount": 88.25,
+				"unit": "g"
+			},
+			{
+				"name": "salt",
+				"amount": 4,
+				"unit": "g"
+			},
+			{
+				"name": "instant yeast",
+				"amount": 1, 
+				"unit": "g"
+			},
+			{
+				"name": "bread flour",
+				"amount": 142.5,
+				"unit": "g"
+			}
+		],
+		"steps": [
+			"Combine water and salt. Mix until dissolved. Then add yeast to mixture (Note: do not let yeast sit in salt mixture long. It will kill the yeast.)",
+			"Add flour to mixture and mix until it forms a rough dough. Continue to knead until it forms a tight dough (around 10 minutes).",
+			"Remove the dough from the mixing bowl and use your hands to form a tight dough ball. It should spring back when pressed.",
+			"Lightly coat ball with olive oil, and place on a flat surface. Cover with plastic wrap, a cloth, or an upturned mixing bowl. Let it rest for 30 min.",
+			"After resting, the dough should be much softer & stretchier. If it passes the window pane test it will make a good pizza.",
+			"Divide into equal dough balls (~236 g each). Put each ball into a lightly oiled, airtight container that will allow for some expansion (~1L containers work best).",
+			"Store dough in the fridge for at least 8 hours, or up to 3 days."
+		]
+	},
+	{
+		"name": "New York Pizza Dough",
+		"ingredients": [
+			{
+				"name": "water",
+				"amount": 105.25,
+				"unit": "g"
+			},
+			{
+				"name": "salt",
+				"amount": 6,
+				"unit": "g",
+			},
+			{
+				"name": "sugar",
+				"amount": 3.5,
+				"unit": "g",
+			},
+			{
+				"name": "instant yeast",
+				"amount": 4.75,
+				"unit": "g",
+			},
+			{
+				"name": "olive oil",
+				"amount": 8,
+				"unit": "g",
+			},
+			{
+				"name": "bread flour",
+				"amount": 159.5,
+				"unit": "g",
+			}
+		],
+		"steps": [
+			"Combine water and salt. Mix until dissolved. Then add yeast to mixture (Note: do not let yeast sit in salt mixture long. It will kill the yeast.)",
+			"Add flour to mixture and mix until it forms a rough dough. Continue to knead until it forms a tight dough (around 10 minutes).",
+			"Remove the dough from the mixing bowl and use your hands to form a tight dough ball. It should spring back when pressed.",
+			"Lightly coat ball with olive oil, and place on a flat surface. Cover with plastic wrap, a cloth, or an upturned mixing bowl. Let it rest for 30 min.",
+			"After resting, the dough should be much softer & stretchier. If it passes the window pane test it will make a good pizza.",
+			"Divide into 4 equal dough balls (~236 g each). Put each ball into a lightly oiled, airtight container that will allow for some expansion (~1L containers work best).",
+			"Store dough in the fridge for at least 8 hours, or up to 3 days."
+		]
+	}
+]
